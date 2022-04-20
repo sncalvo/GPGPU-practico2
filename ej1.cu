132d946a185157a40a7b9561676acc4a3b289faf
Santiago Calvo <sncalvo@hotmail.com>
2022-04-20 00:07:54 -0300

    Separate excercises

--- ej1.cu
+++ ej1.cu
@@ -41,6 +41,18 @@ __global__ void decrypt_kernel(int *d_message, int length)
 	}
 }
 
+
+__global__ void decrypt_kernel_first(int *d_message, int length)
+{
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+  	int stride = blockDim.x * gridDim.x;
+
+	for (int i = index; i < length; i += stride)
+	{
+		d_message[i] = modulo(A_MMI_M * (d_message[i] - B), M);
+	}
+}
+
 __global__ void count_occurences(int *d_message, int occurenses[M], int length)
 {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -52,10 +64,90 @@ __global__ void count_occurences(int *d_message, int occurenses[M], int length)
 	}
 }
 
+
+int parte_a(int length, unsigned int size, int *message)
+{
+	int *d_message;
+	cudaMalloc((void**)&d_message, length * sizeof(int));
+	cudaMemcpy(d_message, message, length * sizeof(int), cudaMemcpyHostToDevice);
+
+	decrypt_kernel_first<<<1, 256>>>(d_message, length);
+	cudaDeviceSynchronize();
+
+	cudaMemcpy(message, d_message, size, cudaMemcpyDeviceToHost);
+
+	cudaFree(d_message);
+
+	return 0;
+}
+
+
+int parte_b(int length, unsigned int size, int *message)
+{
+	int *d_message;
+	cudaMalloc((void**)&d_message, length * sizeof(int));
+	cudaMemcpy(d_message, message, length * sizeof(int), cudaMemcpyHostToDevice);
+
+	// int blockSize = 256;
+	// int numBlocks = length / blockSize;
+
+	decrypt_kernel_first<<<length / 256, 256>>>(d_message, length);
+	cudaDeviceSynchronize();
+
+	cudaMemcpy(message, d_message, size, cudaMemcpyDeviceToHost);
+
+	cudaFree(d_message);
+
+	return 0;
+}
+
+int parte_c(int length, unsigned int size, int *message)
+{
+	int *d_message;
+	cudaMalloc((void**)&d_message, length * sizeof(int));
+	cudaMemcpy(d_message, message, length * sizeof(int), cudaMemcpyHostToDevice);
+
+	int blockSize = 256;
+	int numBlocks = (length + blockSize - 1) / blockSize;
+
+	decrypt_kernel<<<numBlocks, blockSize>>>(d_message, length);
+	cudaDeviceSynchronize();
+
+	cudaMemcpy(message, d_message, size, cudaMemcpyDeviceToHost);
+	cudaFree(d_message);
+
+	return 0;
+}
+
+int parte_2(int length, unsigned int size, int *message, int *occurenses)
+{
+	int *d_message;
+	cudaMalloc((void**)&d_message, length * sizeof(int));
+	cudaMemcpy(d_message, message, length * sizeof(int), cudaMemcpyHostToDevice);
+
+	int *d_occurenses;
+	cudaMalloc((void**)&d_occurenses, M * sizeof(int));
+	cudaMemset(d_occurenses, 0, M * sizeof(int));
+
+	int blockSize = 256;
+	int numBlocks = (size + blockSize - 1) / blockSize;
+
+	decrypt_kernel<<<numBlocks, blockSize>>>(d_message, length);
+	count_occurences<<<numBlocks, blockSize>>>(d_message, d_occurenses, length);
+
+	cudaMemcpy(message, d_message, size, cudaMemcpyDeviceToHost);
+	cudaMemcpy(occurenses, d_occurenses, M * sizeof(int), cudaMemcpyDeviceToHost);
+
+	cudaFree(d_message);
+	cudaFree(d_occurenses);
+
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	int *h_message;
-	int *d_message;
+	// int *d_message;
 	unsigned int size;
 
 	const char * fname;
@@ -77,31 +169,35 @@ int main(int argc, char *argv[])
 	read_file(fname, h_message);
 
 	/* reservar memoria en la GPU */
-	CUDA_CHK(cudaMalloc((void **)&d_message, size));
+	// CUDA_CHK(cudaMalloc((void **)&d_message, size));
 
-	/* copiar los datos de entrada a la GPU */
-	CUDA_CHK(cudaMemcpy(d_message, h_message, size, cudaMemcpyHostToDevice));
+	// /* copiar los datos de entrada a la GPU */
+	// CUDA_CHK(cudaMemcpy(d_message, h_message, size, cudaMemcpyHostToDevice));
 
-	/* Configurar la grilla y lanzar el kernel */
-	// dim3 dimBlock(16, 1);
-	// dim3 dimGrid(size / dimBlock.x, size / dimBlock.y);
-	int blockSize = 256;
-	int numBlocks = (size + blockSize - 1) / blockSize;
+	// /* Configurar la grilla y lanzar el kernel */
+	// // dim3 dimBlock(16, 1);
+	// // dim3 dimGrid(size / dimBlock.x, size / dimBlock.y);
+	// int blockSize = 256;
+	// int numBlocks = (size + blockSize - 1) / blockSize;
 
-	decrypt_kernel<<<numBlocks, blockSize>>>(d_message, length);
+	// decrypt_kernel<<<numBlocks, blockSize>>>(d_message, length);
 
-	int* d_occurenses = (int *)malloc(M * sizeof(int));
+	// int* d_occurenses = (int *)malloc(M * sizeof(int));
 
-	CUDA_CHK(cudaMalloc((void **)&d_occurenses, M * sizeof(int)));
-	CUDA_CHK(cudaMemset(d_occurenses, 0, M * sizeof(int)));
-	count_occurences<<<numBlocks, blockSize>>>(d_message, d_occurenses, length);
+	// CUDA_CHK(cudaMalloc((void **)&d_occurenses, M * sizeof(int)));
+	// CUDA_CHK(cudaMemset(d_occurenses, 0, M * sizeof(int)));
+	// count_occurences<<<numBlocks, blockSize>>>(d_message, d_occurenses, length);
 
-	cudaDeviceSynchronize();
+	// cudaDeviceSynchronize();
 
+	// /* Copiar los datos de salida a la CPU en h_message */
+	// CUDA_CHK(cudaMemcpy(h_message, d_message, size, cudaMemcpyDeviceToHost));
+	// CUDA_CHK(cudaMemcpy(h_occurenses, d_occurenses, M * sizeof(int), cudaMemcpyDeviceToHost));
+	// parte_a(length, size, h_message);
+	// parte_b(length, size, h_message);
+	// parte_c(length, size, h_message);
 	int* h_occurenses = (int *)malloc(M * sizeof(int));
-	/* Copiar los datos de salida a la CPU en h_message */
-	CUDA_CHK(cudaMemcpy(h_message, d_message, size, cudaMemcpyDeviceToHost));
-	CUDA_CHK(cudaMemcpy(h_occurenses, d_occurenses, M * sizeof(int), cudaMemcpyDeviceToHost));
+	parte_2(length, size, h_message, h_occurenses);
 
 	// setlocale(LC_ALL, "en_US.UTF-8");
 	// despliego el mensaje
@@ -132,7 +228,7 @@ int main(int argc, char *argv[])
 	// fwprintf(res_file, L"\n");
 
 	// libero la memoria en la GPU
-	CUDA_CHK(cudaFree(d_message));
+	// CUDA_CHK(cudaFree(d_message));
 
 	// fclose(res_file);
 	// libero la memoria en la CPU
