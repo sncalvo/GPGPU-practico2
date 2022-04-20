4041d94970ea49adc7d545691fd542a2664f8303
Santiago Calvo <sncalvo@hotmail.com>
2022-04-20 10:18:01 -0300

    Add cleaner solution

--- ej1.cu
+++ ej1.cu
@@ -88,9 +88,6 @@ int parte_b(int length, unsigned int size, int *message)
 	cudaMalloc((void**)&d_message, length * sizeof(int));
 	cudaMemcpy(d_message, message, length * sizeof(int), cudaMemcpyHostToDevice);
 
-	// int blockSize = 256;
-	// int numBlocks = length / blockSize;
-
 	decrypt_kernel_first<<<length / 256, 256>>>(d_message, length);
 	cudaDeviceSynchronize();
 
@@ -144,18 +141,36 @@ int parte_2(int length, unsigned int size, int *message, int *occurenses)
 	return 0;
 }
 
+void print_occurences(int *occurenses)
+{
+	for (int i = 0; i < 256; i++)
+	{
+		printf("%d: %d\n", i, occurenses[i]);
+	}
+}
+void print_message(int *message, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		printf("%c", (char)message[i]);
+	}
+	printf("\n");
+}
+
 int main(int argc, char *argv[])
 {
 	int *h_message;
 	// int *d_message;
 	unsigned int size;
 
-	const char * fname;
+	const char *fname;
+	const char *ejercicio;
 
-	if (argc < 2) {
-		printf("Debe ingresar el nombre del archivo\n");
+	if (argc < 3) {
+		printf("Debe ingresar el nombre del archivo y el ejercicio (1a, 1b, 1c, 2)\n");
 	} else {
 		fname = argv[1];
+		ejercicio = argv[2];
 	}
 
 	int length = get_text_length(fname);
@@ -168,70 +183,23 @@ int main(int argc, char *argv[])
 	// leo el archivo de la entrada
 	read_file(fname, h_message);
 
-	/* reservar memoria en la GPU */
-	// CUDA_CHK(cudaMalloc((void **)&d_message, size));
+	printf("Ejecutando ejercicio: %s\n", ejercicio);
 
-	// /* copiar los datos de entrada a la GPU */
-	// CUDA_CHK(cudaMemcpy(d_message, h_message, size, cudaMemcpyHostToDevice));
-
-	// /* Configurar la grilla y lanzar el kernel */
-	// // dim3 dimBlock(16, 1);
-	// // dim3 dimGrid(size / dimBlock.x, size / dimBlock.y);
-	// int blockSize = 256;
-	// int numBlocks = (size + blockSize - 1) / blockSize;
-
-	// decrypt_kernel<<<numBlocks, blockSize>>>(d_message, length);
-
-	// int* d_occurenses = (int *)malloc(M * sizeof(int));
-
-	// CUDA_CHK(cudaMalloc((void **)&d_occurenses, M * sizeof(int)));
-	// CUDA_CHK(cudaMemset(d_occurenses, 0, M * sizeof(int)));
-	// count_occurences<<<numBlocks, blockSize>>>(d_message, d_occurenses, length);
-
-	// cudaDeviceSynchronize();
-
-	// /* Copiar los datos de salida a la CPU en h_message */
-	// CUDA_CHK(cudaMemcpy(h_message, d_message, size, cudaMemcpyDeviceToHost));
-	// CUDA_CHK(cudaMemcpy(h_occurenses, d_occurenses, M * sizeof(int), cudaMemcpyDeviceToHost));
-	// parte_a(length, size, h_message);
-	// parte_b(length, size, h_message);
-	// parte_c(length, size, h_message);
-	int* h_occurenses = (int *)malloc(M * sizeof(int));
-	parte_2(length, size, h_message, h_occurenses);
-
-	// setlocale(LC_ALL, "en_US.UTF-8");
-	// despliego el mensaje
-	// FILE *res_file = fopen("test.txt", "w");
-	// fwrite(h_message, sizeof(int), length, res_file);
-	for (int i = 0; i < length; i++) {
-		// if (h_message[i] < 32 || h_message[i] > 126) {
-		// 	fprintf(res_file, "%c", (short)h_message[i]);
-		// } else {
-		// 	fprintf(res_file, "%c", (char)h_message[i]);
-		// }
-		// fwprintf(res_file, L"%c", (char)h_message[i]);
-		printf("%c", (char)h_message[i]);
+	if (strcmp(ejercicio, "1a")) {
+		parte_a(length, size, h_message);
+		print_message(h_message, length);
+	} else if (strcmp(ejercicio, "1b")) {
+		parte_b(length, size, h_message);
+		print_message(h_message, length);
+	} else if (strcmp(ejercicio, "1c")) {
+		parte_c(length, size, h_message);
+		print_message(h_message, length);
+	} else if (strcmp(ejercicio, "2")) {
+		int *h_occurenses = (int *)malloc(M * sizeof(int));
+		parte_2(length, size, h_message, h_occurenses);
+		print_occurences(h_occurenses);
+		free(h_occurenses);
 	}
-
-	printf("\n");
-	for (int i = 0; i < M; i++) {
-		// if (h_message[i] < 32 || h_message[i] > 126) {
-		// 	fprintf(res_file, "%c", (short)h_message[i]);
-		// } else {
-		// 	fprintf(res_file, "%c", (char)h_message[i]);
-		// }
-		// fwprintf(res_file, L"%c", (char)h_message[i]);
-		printf("%d: %d\n" , i, h_occurenses[i]);
-	}
-	printf("\n");
-
-	// fwprintf(res_file, L"\n");
-
-	// libero la memoria en la GPU
-	// CUDA_CHK(cudaFree(d_message));
-
-	// fclose(res_file);
-	// libero la memoria en la CPU
 	free(h_message);
 
 	return 0;
